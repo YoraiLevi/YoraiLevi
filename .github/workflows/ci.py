98c8a945c180bcee0786da8fa9921bc669ceeee9
Yorai Levi <yorai.email@gmail.com>
2022-12-19 07:44:06 +0200

    Update ci.py

--- .github/workflows/ci.py
+++ .github/workflows/ci.py
@@ -3,9 +3,9 @@ import subprocess
 from pathlib import Path
 
 for pyfile in glob("*.py"):
-    subp = subprocess.run(["python3",pyfile],capture_output=True)
+    subp = subprocess.run(["python3",pyfile],capture_output=True,check=True)
     p = Path(pyfile)
     with open(p.stem,"wb+") as f:
         f.write(subp.stdout)
         if(subp.stderr):
-            print(str(subp.stderr))
+            print(subp.stderr)
